62d1647e57948df4520a30956516411f49adb6fd
Jean-Pierre Höhmann <jp@hhmn.de>
2016-10-14 23:00:22 +0200

    Remove user interface

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -68,16 +68,6 @@ var fallbackStreamOpts = {
 };
 
 /**
- * Set a color scheme.
- * This will apply a given color scheme to the app.
- */
-var colorize = (colorScheme) => {
-    var e = document.getElementsByTagName('nav')[0];
-    e.style.backgroundColor = '#' + colorScheme[1];
-    e.className = colorScheme[2] ? 'inverted' : '';
-}
-
-/**
  * This will initialize the whiteboard on the canvas from a given snapshot.
  */
 var mkCanvas = (snapshot) => {
@@ -93,31 +83,18 @@ var mkCanvas = (snapshot) => {
 
 window.onload = () => {
     var body = document.getElementsByTagName('body')[0];
-    var leftBtn = document.getElementById('left-btn');
-    var rightBtn = document.getElementById('right-btn');
 
     // Manually calculate the size of some elements to work around flexbox 
     // issues and limitations.
     var f = () => {
         if (!fakeResize) {
-            document.getElementById('content').style.height
-                = window.innerHeight 
-                - document.getElementsByTagName('nav')[0].offsetHeight 
-                + 'px';
 
             // Wait for transitions.
             setTimeout(() => {
-                var e = document.getElementById('main');
-                var _ = document.getElementById('presentation');
-                if (innerWidth > innerHeight) {
-                    _.style.height = '100%';
-                    _.style.width = '0';
-                    _.style.width = e.clientWidth - 224 + 'px';
-                } else {
-                    _.style.width = '100%';
-                    _.style.height = '0';
-                    _.style.height = e.clientHeight - 224 + 'px';
-                }
+                /*
+                 * There is currently no code here, but this will be needed 
+                 * again in the future.
+                 */
 
                 // Pass another resize for Literallycanvas.
                 fakeResize = true;
@@ -129,114 +106,6 @@ window.onload = () => {
     }
     window.addEventListener('throttledResize', f);
     f();
-
-    // Event handlers for the buttons in the menu bar.
-    leftBtn.addEventListener(
-            'click', 
-            () => {
-                body.classList.toggle('left-expanded');
-                dispatchEvent(new Event('resize'));
-            });
-    rightBtn.addEventListener(
-            'click', 
-            () => {
-                body.classList.toggle('right-expanded');
-                dispatchEvent(new Event('resize'));
-            });
-    document.getElementById('back-btn').addEventListener(
-            'click', 
-            () => body.className = '');
-    document.getElementById('users-btn').addEventListener(
-            'click', 
-            () => {
-                document
-                    .getElementsByTagName('main')[0]
-                    .classList
-                    .toggle('listview');
-            });
-
-    // If on mobile clicking in the center area will close any <aside>s.
-    var _ = document.getElementsByClassName('center');
-    for (var i = 0; i < _.length; i++) {
-        _[i].addEventListener(
-                'click',
-                (_) => {
-                    if (body.className != '' && window.innerWidth <= 600) {
-                        body.className = '';
-                        leftBtn.classList.remove('active');
-                        rightBtn.classList.remove('active');
-                        _.stopPropagation();
-                    }
-                    else if (body.classList.contains('right-expanded') 
-                            && window.innerWidth <= 1280) {
-                        body.classList.remove('right-expanded');
-                        rightBtn.classList.remove('active');
-                        _.stopPropagation();
-                    }
-                },
-                true);
-    }
-
-    // Clicking a button will toggle its active state.
-    var _ = document.getElementsByClassName('toggle-btn');
-    for (var i = 0; i < _.length; i++) {
-        _[i].addEventListener('click', 
-                (_) => _.currentTarget.classList.toggle('active'));
-    }
-
-    // List of color schemes.
-    var colors = [
-        ['Red', 'f44336', true],
-        ['Pink', 'e91e63', true],
-        ['Purple', '9c27b0', true],
-        ['Deep Purple', '673ab7', true],
-        ['Indigo', '3f51b5', true],
-        ['Blue', '2196f3', true],
-        ['Light Blue', '03a9f4', false],
-        ['Cyan', '00bcd4', false],
-        ['Teal', '009688', true],
-        ['Green', '4caf50', false],
-        ['Light Green', '8bc34a', false],
-        ['Lime', 'cddc39', false],
-        ['Yellow', 'ffeb3b', false],
-        ['Amber', 'ffc107', false],
-        ['Orange', 'ff9800', false],
-        ['Deep Orange', 'ff5722', true],
-        ['Brown', '795548', true],
-        ['Blue Grey', '607d8b', true]];
-
-    // Add buttons for the color schemes.
-    for (var i = 0; i < colors.length; i++) {
-        var _ = document.createElement('a');
-        _.textContent = colors[i][0];
-        _.addEventListener(
-                'click',
-                ((_) => () => { 
-                    colorize(_);
-                    localStorage.setItem('colorScheme', JSON.stringify(_));
-                })(colors[i]));
-        var e = document.createElement('p');
-        document.getElementById('colors').appendChild(e);
-        e.appendChild(_);
-    }
-
-    // Event hander for the buttons that control the whiteboard.
-    document.getElementById('presentation-toggle').addEventListener('click', () => {
-        document.getElementById('presentation-toggle').textContent 
-            = document.getElementById('presentation').classList.toggle('hidden')
-            ? 'Open whiteboard' : 'Close whiteboard';
-        dispatchEvent(new Event('resize'));
-    });
-    document
-        .getElementById('whiteboard-clear')
-        .addEventListener('click', () => {
-            canvas.clear();
-            localStream.sendData({type: 'canvas-clear'})
-        });
-
-    // Fetch color scheme.
-    var _ = localStorage.getItem('colorScheme');
-    if (_) { colorize(JSON.parse(_)); }
 
     // Prepare a request for a room token.
     var req = new XMLHttpRequest();
