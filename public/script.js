442886cbc82c30e5cf75c06bea4760738a1dbe7d
Jean-Pierre Höhmann <jp@hhmn.de>
2017-01-19 18:47:33 +0100

    Add demo app main script.

---
+++ public/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+ * Tanura demo app.
+ *
+ * This is the main script of the app demonstrating Tanura's features.
+ */
+
+/**
+ * Bootstrapping routine.
+ */
+window.onload = () => {
+    console.log("Tanura demo.");
+}
+
