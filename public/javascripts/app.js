e90fb9c0e57268766fa4b35630ca3fdccf352165
Jean-Pierre Höhmann <jp@hhmn.de>
2017-01-31 20:56:17 +0100

    Add tanura.eventHandler.register().

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,3 +18,13 @@ tanura.eventHandler.fire = (x) =>
             || ! _[x].forEach
             || _[x].forEach((i) => i())))(tanura.eventHandler.events);
 
+/*
+ * Register a callback for an event.
+ *
+ * This will add a function to be called whenever a given event occurs. Exit is 
+ * true on success, false otherwise.
+ */
+tanura.eventHandler.register = (x, f) =>
+    ((_) =>
+        ! (! _[x] || ! _[x].push || _[x].push(f)))(tanura.eventHandler.events);
+
