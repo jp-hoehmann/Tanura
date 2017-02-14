91011f37c4fc76433b1f291c539a3d7af335d28c
Jean-Pierre Höhmann <jp@hhmn.de>
2017-02-14 22:43:04 +0100

    Add tanura.log().

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -6,3 +6,10 @@
  * This script contains miscellaneous helpers used throughout Tanura's code.
  */
 
+/*
+ * Log something.
+ *
+ * For now, this is just aliased to console.log.
+ */
+tanura.log = console.log;
+
