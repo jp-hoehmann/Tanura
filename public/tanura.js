0fb42aa8dcfee7df24f9a967f671c16663a87477
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-07 18:47:01 +0100

    Add tanura.events.whiteboard-created[].

--- public/tanura.js
+++ public/tanura.js
@@ -19,6 +19,7 @@ var tanura = {
         events: {
             init: [],
             whiteboard-cleared: [],
+            whiteboard-created: [],
             whiteboard-loaded: []
         }
     },
