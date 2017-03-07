4ad734512ec1db6f833d4c0f160fcb5a02829196
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-07 21:48:09 +0100

    Add tanura.events.media-failed[].

--- public/tanura.js
+++ public/tanura.js
@@ -24,6 +24,7 @@ var tanura = {
             localstream-created: [],
             localstream-failed: [],
             media-opened: [],
+            media-failed: [],
             stream-added: [],
             stream-opened: [],
             stream-removed: [],
