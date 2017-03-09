e041588eeb49ebae7bcdb2b332f6ee2743ddcc91
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-09 21:27:35 +0100

    Move event throttling to helper.js.

--- public/javascripts/helper.js
+++ public/javascripts/helper.js
@@ -16,4 +16,26 @@
      */
     _.log = ((_) => (x) => console.log("[" + _ + "] " + x))(_.info.name);
 
+    /*
+     * Throttle an event.
+     *
+     * This will limit the rate of a DOM event, to allow attaching heavier 
+     * handlers.
+     */
+    _.throttle = (type, name, obj) => {
+        obj = obj || window;
+        var running = false;
+        var f = () => {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(() => {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, f);
+    }
+
+    _.throttle('resize', 'throttledResize');
+
 })();
