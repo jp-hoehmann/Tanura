8d57ca13720367bc8d63eabef16f3d64399cb963
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-21 18:33:22 +0100

    Give the user the option to bind event callbacks.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -39,9 +39,10 @@ tanura.eventHandler.fire = (_, x) =>
 /*
  * Register a callback for an event.
  *
- * This will add a function to be called whenever a given event occurs. Exit is 
- * true on success, false otherwise.
+ * This will add a function to be called whenever a given event occurs.  Exit is 
+ * true on success, false otherwise.  If an additional parameter is provided, 
+ * the callback is bound to it, otherwise it will be bound to Tanura.
  */
-tanura.eventHandler.register = (_, f) =>
-    tanura.eventHandler.safe(_, (_) => _.push(f));
+tanura.eventHandler.register = (_, f, x) =>
+    tanura.eventHandler.safe(_, (_) => _.push(f.bind(x || tanura)));
 
