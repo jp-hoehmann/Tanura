772236bc165474449a2300f04d95c31af3d6dfca
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-28 18:17:23 +0200

    Fire media_granted.

--- public/javascripts/lib.js
+++ public/javascripts/lib.js
@@ -9,7 +9,27 @@
 
 (function() {
 
-    // TODO
+    /*
+     * Group a list of events into another event.
+     *
+     * This will fire a given event whenever an event from a given list fires.  
+     * The return value will be true, if the grouping succeeded, false 
+     * otherwise. If the grouping fails due to some of the grouped events not 
+     * existing the rest will still be grouped.
+     */
+    var groupEvents = function(_, l) {
+        var r = true;
+        for (var i of l) {
+            if (! this.register(i, function(x) {
+                return this.fire(_, x);
+            }, this)) { r = false; }
+        }
+        return r;
+    }.bind(tanura.eventHandler);
+
+    groupEvents(
+            'media_granted',
+            ['media_granted_preferred', 'media_granted_fallback']);
 
 })();
 
