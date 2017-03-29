1013884fe32edec817535953b9a5169cc33b7441
Jean-Pierre Höhmann <jp@hhmn.de>
2017-03-29 18:02:39 +0200

    Create porcelain script.

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+ * Tanura built-in behavior.
+ *
+ * This script defines Tanura's business logic in the form of built-in routines 
+ * for handling certain events.
+ */
+
+(function() {
+
+    // TODO
+
+})()
+
